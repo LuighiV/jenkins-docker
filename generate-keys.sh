9eb14bc8ac64ca5380e9905a4ca9bd3380502b7d
root <root@terraform-instance.us-central1-c.c.jenkins-continuous-deploy.internal>
2021-01-21 02:33:54 +0000

    Changes in docker-compose and generate keys script

--- generate-keys.sh
+++ generate-keys.sh
@@ -2,15 +2,15 @@
 
 if [ -z "$1" ]
   then
-    echo "No domain name supplied replaced by localhost"
-    KEY_NAME=localhost
+    echo "No domain name supplied replaced by ssh_keys"
+    KEY_NAME=ssh_keys
   else
     KEY_NAME=$1
 fi
 
 # Generating keys inside the container
-sudo docker exec -it jenkins-blueocean ssh-keygen -t rsa /var/jenkins_home/.ssh/KEY_NAME -C jenkins
+sudo docker exec jenkins-blueocean /bin/bash -c "ssh-keygen -t rsa -f /var/jenkins_home/.ssh/$KEY_NAME -C jenkins"
 
 # Copying the keys to the host machine
 mkdir -p .generated
-sudo docker cp jenkins-blueocean:/var/jenkins_home/.ssh/KEY_NAME.pub .generated/
+sudo docker cp jenkins-blueocean:/var/jenkins_home/.ssh/$KEY_NAME.pub .generated/
